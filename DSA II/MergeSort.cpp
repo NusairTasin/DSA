6e7c5b930b3ad280848d0c9473cbece9ce844041
NusairTasin <tasinnusair@gmail.com>
2025-03-21 00:09:10 +0600

    Updated MergeSort & QuickSort

--- DSA II/MergeSort.cpp
+++ DSA II/MergeSort.cpp
@@ -1,3 +1,27 @@
+/*
+    l = left index of the array
+    r = right index of the array
+    m = middle index of the array
+
+    MergeSort Function:
+        1. If l < r, then
+            a. Calculate m = (l+r)/2
+            b. MergeSort(arr, l, m)
+            c. MergeSort(arr, m+1, r)
+            d. Merge(arr, l, m, r)
+
+    Merge Function:
+        1. n1 = m-l+1
+        2. n2 = r-m
+        3. Create two arrays L and R of size n1 and n2 respectively
+        4. Copy the elements of arr[l...m] to L[0...n1-1] and arr[m+1...r] to R[0...n2-1]
+        5. L[n1] = INT_MAX
+        6. R[n2] = INT_MAX
+        7. i = 0, j = 0
+        8. For k = l to r
+            a. If L[i] <= R[j], then arr[k] = L[i] and i++
+            b. Else arr[k] = R[j] and j++
+*/
 #include <iostream>
 using namespace std;
 
--- DSA II/QuickSort.cpp
+++ DSA II/QuickSort.cpp
@@ -1,6 +1,56 @@
+/*  
+    l = left index of the array
+    r = right index of the array
+
+    QuickSort Function:
+        1. if l < r, then
+            a. p = Partition(arr, l, r)
+            b. QuickSort(arr, l, p-1)
+            c. QuickSort(arr, p+1, r)
+    
+    Partition Function:
+        1. pivot = arr[r]
+        2. i = l-1
+        3. for j = l to r-1
+            a. if arr[j] < pivot, then
+                i++
+                swap arr[i] and arr[j]
+        4. swap arr[i+1] and arr[r]
+        5. return i+1
+*/
 #include<iostream>
 using namespace std;
 
+int partition(int A[], int p, int r) {
+    int x = A[r];
+    int i = p-1;
+    for(int j = p; j < r; j++) {
+        if(A[j] <= x) {
+            i++;
+            swap(A[i], A[j]);
+        }
+    }
+    swap(A[i+1], A[r]);
+    return i+1;
+}
+
+void quickSort(int A[], int p, int r) {
+    if(p < r) {
+        int q = partition(A,p,r);
+        quickSort(A, p, q-1);
+        quickSort(A, q+1, r);
+    }
+}
+
 int main() {
+    int A[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(A) / sizeof(A[0]);
+    quickSort(A, 0, n - 1);
     
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+    return 0;
 }
