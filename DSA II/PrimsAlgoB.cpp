ab8c0e60a44b04f1f50873bfbf19891fbad079c6
NusairTasin <tasinnusair@gmail.com>
2025-04-22 19:32:16 +0600

    Fixed Prim's

--- DSA II/PrimsAlgoB.cpp
+++ DSA II/PrimsAlgoB.cpp
@@ -1,56 +1,71 @@
-//Prim's Algorithm (Minimum Spanning Tree)
-//Beginner Friendly using Adjacency Matrix
+//Prim's Algorithm
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int minVertex(vector<int>& key, vector<bool>& explored, int V) {
-    int min = INT_MAX, minIndex;
+const int V = 9;
+int wght = 0;
 
-    for(int v=0; v<V; v++) {
-        if(!explored[v] && key[v]<min) {
-            min = key[v];
-            minIndex = v;
+int minWeight(int weight[], bool visited[]){
+    int minimum = INT_MAX;
+    int min_index; 
+    for(int i = 0; i < V; i++){
+        if(visited[i] == false and weight[i] < minimum){
+            minimum = weight[i];
+            min_index = i;
         }
     }
-    return minIndex;
+    return min_index;
 }
 
-void primsAlgo(vector<vector<int>> graph, int V) {
-    vector<int> parent(V);
-    vector<int> key(V, INT_MAX);
-    vector<bool> explored(V, false);
+void printMST(int parent[], int graph[V][V]){
+    cout << "Edge \tWeight\n";
+    for(int i = 1; i < V; i++){
+        cout << parent[i] << " - " << i << "\t" << graph[i][parent[i]] << endl;
+        wght += graph[i][parent[i]];
+    }
 
-    key[0] = 0;
+    cout << "Total weight of MST: " << wght << endl;
+    
+}
+
+void primMST(int graph[V][V]){
+    int parent[V];
+    int weight[V];
+    bool visited[V];
+    for(int i =0; i < V; i++){
+        visited[i] = false;
+        weight[i] = INT_MAX;
+        parent[i] = -1;
+    }
+
+    weight[0] = 0;
     parent[0] = -1;
-
-    for(int count=0; count<V; count++) {
-        int u = minVertex(key, explored, V);
-        explored[u] = true;
-
-        for(int v = 0; v < V; v++) {
-            if(graph[u][v] && !explored[v] && graph[u][v]<key[v]) {
+    
+    for(int count = 0; count <V -1; count ++){
+        int u = minWeight(weight, visited);
+        visited[u] = true;
+        for(int v=0; v < V; v++){
+            if (graph[u][v] != 0 && visited[v] == false && graph[u][v] < weight[v]) {
                 parent[v] = u;
-                key[v] = graph[u][v];
+                weight[v] = graph[u][v];
             }
         }
     }
-
-    //Print
-    cout<<"Edge \tWeight\n";
-    for(int i=1; i<V; i++) {
-        cout<<parent[i]<<"-"<<i<<"\t"<<graph[i][parent[i]]<<"\n";
-    }
+    printMST(parent, graph);
 }
 
-int main() {
-    int V = 5;
-    vector<vector<int>> graph = {
-        {0, 2, 0, 6, 0},
-        {2, 0, 3, 8, 5},
-        {0, 3, 0, 0, 7},
-        {6, 8, 0, 0, 9},
-        {0, 5, 7, 9, 0}
+int main(){
+    int graph[V][V] ={
+        {0, 10, 6, 5, 0, 0, 0, 0, 0}, 
+        {10, 0, 0, 15, 8, 0, 0, 0, 0}, 
+        {6, 0, 0, 4, 0, 0, 6, 0, 0}, 
+        {5, 15, 4, 0, 0, 0, 0, 0, 0}, 
+        {0, 8, 0, 0, 0, 0, 3, 0, 0}, 
+        {0, 0, 0, 0, 0, 0, 4, 7, 6}, 
+        {0, 0, 6, 0, 3, 4, 0, 0, 0}, 
+        {0, 0, 0, 0, 0, 7, 0, 0, 0}, 
+        {0, 0, 0, 0, 0, 6, 0, 0, 0}
     };
-    primsAlgo(graph, V);
+    primMST(graph);
 }
